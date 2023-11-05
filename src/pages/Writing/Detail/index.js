e655aba68a66e91451ab804f1331ca41f7a6ca83
yangqi06 <yangqi06@kuaishou.com>
2023-11-05 16:02:45 +0800

    feat: 兜底判断

--- src/pages/Writing/Detail/index.js
+++ src/pages/Writing/Detail/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./index.less";
 import HTTP from "../../../utils/api.js";
-import moment from 'moment';
+import moment from "moment";
 import {
   Form,
   Input,
@@ -22,13 +22,15 @@ import { getQueryString } from "../../../utils/stringUtils";
 import Header from "../../../components/Header/index.js";
 
 const { TextArea } = Input;
-const dateFormat = 'YYYY-MM-DD HH:mm:ss';
+const dateFormat = "YYYY-MM-DD HH:mm:ss";
 const content1 = (
-    <div className="popColor">
-        <div>①每个练习类型的作文任务有三次智能批改次数，请合理运用。</div>
-        <div>②作文任务到达截止时间将会自动提交，记得及时完成作文任务。</div>
-        <div>③作文提交后，将无法再进行编辑和修改，包括智能批改功能。如需保存当前作文进度，并在以后继续修改，请选择“保存并退出”。</div>
+  <div className="popColor">
+    <div>①每个练习类型的作文任务有三次智能批改次数，请合理运用。</div>
+    <div>②作文任务到达截止时间将会自动提交，记得及时完成作文任务。</div>
+    <div>
+      ③作文提交后，将无法再进行编辑和修改，包括智能批改功能。如需保存当前作文进度，并在以后继续修改，请选择“保存并退出”。
     </div>
+  </div>
 );
 let autoSaveTimer = {};
 
@@ -51,45 +53,46 @@ export default class WritingDetail extends React.Component {
         maximum: 160,
         minimum: 120,
       },
-      activeKey: '2',
+      activeKey: "2",
       aiDetectionTimes: 1, // 可使用的ai检测次数，为0时禁止使用
       isSubmit: false, // 是否已提交
       aiReview: {
-            "aiComment": "作者词汇量稍有不足，请注意高级词汇积累；复杂句使用不错，句法规范；文章衔接不够流畅内容逻辑顺畅；作文内容点跟作文要求相去甚远，请注意内容覆盖作文要求。请注意方法，勤加练习。",
-            "aiContentScore": 74.85,
-            "aiDetectionTimes": 1,
-            "aiScore": 59.53,
-            "aiSentenceScore": 73.91,
-            "aiStructureScore": 38.46,
-            "aiVocabularyScore": 58.93,
-            "sentenceComments": [
-                {
-                    "sentence": "That is my grandfather's traditional Chinese medicine pharmacy, affectionately referred to by patients as the 'apricot forest'.",
-                    "suggestions": [
-                        "请检查the 'apricot，可能存在大小写拼写建议或空格错误，建议替换为the'apricot。"
-                    ]
-                },
-                {
-                    "sentence": "I can feel as if there is an apricot forest behind this pharmacy, shining brightly under the sunlight.",
-                    "suggestions": [
-                        "请检查under，可能存在介词错误，建议替换为in。"
-                    ]
-                },
-                {
-                    "sentence": "Grandfather said, \"This is saving people, and it's also saving oneself\".",
-                    "suggestions": [
-                        "请检查, \"This，可能存在大小写拼写建议或空格错误，建议替换为,\" This。"
-                    ]
-                },
-                {
-                    "sentence": "I think this medicine juice, which is boiled into three mouthfuls with three bowls of water, is exactly where the precious essence of Chinese medicine lies: redemption and self redemption",
-                    "suggestions": [
-                        "请检查是否有标点错误，建议新增.。"
-                    ]
-                }
-            ]
-        },
-      score:12, // -1 为未考试
+        aiComment:
+          "作者词汇量稍有不足，请注意高级词汇积累；复杂句使用不错，句法规范；文章衔接不够流畅内容逻辑顺畅；作文内容点跟作文要求相去甚远，请注意内容覆盖作文要求。请注意方法，勤加练习。",
+        aiContentScore: 74.85,
+        aiDetectionTimes: 1,
+        aiScore: 59.53,
+        aiSentenceScore: 73.91,
+        aiStructureScore: 38.46,
+        aiVocabularyScore: 58.93,
+        sentenceComments: [
+          {
+            sentence:
+              "That is my grandfather's traditional Chinese medicine pharmacy, affectionately referred to by patients as the 'apricot forest'.",
+            suggestions: [
+              "请检查the 'apricot，可能存在大小写拼写建议或空格错误，建议替换为the'apricot。",
+            ],
+          },
+          {
+            sentence:
+              "I can feel as if there is an apricot forest behind this pharmacy, shining brightly under the sunlight.",
+            suggestions: ["请检查under，可能存在介词错误，建议替换为in。"],
+          },
+          {
+            sentence:
+              'Grandfather said, "This is saving people, and it\'s also saving oneself".',
+            suggestions: [
+              '请检查, "This，可能存在大小写拼写建议或空格错误，建议替换为," This。',
+            ],
+          },
+          {
+            sentence:
+              "I think this medicine juice, which is boiled into three mouthfuls with three bowls of water, is exactly where the precious essence of Chinese medicine lies: redemption and self redemption",
+            suggestions: ["请检查是否有标点错误，建议新增.。"],
+          },
+        ],
+      },
+      score: 12, // -1 为未考试
       examType: "", // practice
       submitTimes: 1, // 提交次数
       autoSaveTime: "",
@@ -101,7 +104,7 @@ export default class WritingDetail extends React.Component {
   }
 
   componentWillUnmount() {
-    clearInterval(autoSaveTimer) 
+    clearInterval(autoSaveTimer);
   }
   // 初始化
   inited() {
@@ -135,9 +138,12 @@ export default class WritingDetail extends React.Component {
           examType: res?.data?.data?.examType,
           aiDetectionTimes: res?.data?.data?.aiDetectionTimes, // 可使用的ai检测次数，为0时禁止使用
           isSubmit: res?.data?.data?.isSubmit, // 是否已提交
-        },);
-        if(!res?.data?.data?.isSubmit){
-            autoSaveTimer = setInterval(this.handleWritingSave.bind(this), 1000 * 60);
+        });
+        if (!res?.data?.data?.isSubmit) {
+          autoSaveTimer = setInterval(
+            this.handleWritingSave.bind(this),
+            1000 * 60
+          );
         }
       })
       .catch((err) => {
@@ -151,14 +157,14 @@ export default class WritingDetail extends React.Component {
       paperId,
       title,
       content,
-      isSubmit: true
+      isSubmit: true,
     })
       .then((res) => {
-        if(res.data.state !== 0){
-            message.error(`提交失败:${res.data.msg}`);
-        }else{
-            message.success(`提交成功!`);
-            this.getWritingDetail();
+        if (res.data.state !== 0) {
+          message.error(`提交失败:${res.data.msg}`);
+        } else {
+          message.success(`提交成功!`);
+          this.getWritingDetail();
         }
       })
       .catch((err) => {
@@ -166,48 +172,53 @@ export default class WritingDetail extends React.Component {
       });
   }
   // 保存作文
-  handleWritingSave(){
-    console.log('handleWritingSave')
+  handleWritingSave() {
+    console.log("handleWritingSave");
     const { paperId, title, content } = this.state;
     this.setState({
-        autoSaveTime: moment().format(dateFormat)
-    })
+      autoSaveTime: moment().format(dateFormat),
+    });
     return HTTP.post("/stu-writing-exam/submit", {
       paperId,
       title,
       content,
-      isSubmit: false
-    })
+      isSubmit: false,
+    });
   }
   // 保存并退出
-  handleWritingSaveBack(){
-    this.handleWritingSave().then((res) => {
-        clearInterval(autoSaveTimer)
-        window.history.go(-1)
+  handleWritingSaveBack() {
+    this.handleWritingSave()
+      .then((res) => {
+        clearInterval(autoSaveTimer);
+        window.history.go(-1);
       })
       .catch((err) => {
         message.error("保存失败!");
       });
   }
-  handleTabChange(val){
+  handleTabChange(val) {
     this.setState({
-        activeKey: val
-    })
+      activeKey: val,
+    });
   }
   // ai阅卷
-  getAiReview(){
+  getAiReview() {
     const { paperId, title, content } = this.state;
     HTTP.post(`/stu-writing-exam/ai-review`, {
-        paperId,
-        title,
-        content,
+      paperId,
+      title,
+      content,
     })
       .then((res) => {
-        this.setState({
-          aiReview: res?.data?.data,
-          aiDetectionTimes: res?.data?.data?.aiDetectionTimes,
-          activeKey: '1',
-        });
+        if (res.data.state === 0) {
+          this.setState({
+            aiReview: res?.data?.data,
+            aiDetectionTimes: res?.data?.data?.aiDetectionTimes,
+            activeKey: "1",
+          });
+        } else {
+          message.error(res.data.msg);
+        }
       })
       .catch((err) => {
         message.error("ai阅卷失败!");
@@ -244,7 +255,7 @@ export default class WritingDetail extends React.Component {
               <div className="errorItemContent">
                 <div className="errorItemContent">{item.sentence}</div>
                 {item.suggestions.map((suggestion) => {
-                    return (<div className="errorItemContent">{suggestion}</div>)
+                  return <div className="errorItemContent">{suggestion}</div>;
                 })}
               </div>
             </div>
@@ -255,26 +266,38 @@ export default class WritingDetail extends React.Component {
   }
 
   render() {
-    const { paperId, title, content, writing, examType, aiDetectionTimes,isSubmit, aiReview, autoSaveTime, activeKey } = this.state;
+    const {
+      paperId,
+      title,
+      content,
+      writing,
+      examType,
+      aiDetectionTimes,
+      isSubmit,
+      aiReview,
+      autoSaveTime,
+      activeKey,
+    } = this.state;
     return (
       <div className="writing-detail-container">
         <Header />
         <div className="writing-header">
-          <div className="header-back" onClick={this.handleWritingSaveBack.bind(this)}>
+          <div
+            className="header-back"
+            onClick={this.handleWritingSaveBack.bind(this)}
+          >
             <LeftCircleOutlined />
-            <div className="back-text">
-              保存并退出
-            </div>
+            <div className="back-text">保存并退出</div>
           </div>
           <div className="header-tips">
             <Tooltip
-                title={content1}
-                trigger="hover"
-                placement="top"
-                color="rgba(0, 0, 0, 0.7)"
-                overlayStyle={{ minWidth: '440px' }}
+              title={content1}
+              trigger="hover"
+              placement="top"
+              color="rgba(0, 0, 0, 0.7)"
+              overlayStyle={{ minWidth: "440px" }}
             >
-                <InfoCircleOutlined />
+              <InfoCircleOutlined />
             </Tooltip>
             <div className="tips-text">温馨提示</div>
           </div>
@@ -286,7 +309,9 @@ export default class WritingDetail extends React.Component {
                 <div className="fir-word-num">
                   词数统计：{this.computedTextCount(content)}词
                 </div>
-                <div className="fir-save">{autoSaveTime ? `${autoSaveTime}已自动保存` : ''} </div>
+                <div className="fir-save">
+                  {autoSaveTime ? `${autoSaveTime}已自动保存` : ""}{" "}
+                </div>
               </div>
               <div className="left-sec">
                 <Input
@@ -305,49 +330,52 @@ export default class WritingDetail extends React.Component {
                   value={content}
                   bordered={false}
                   disabled={isSubmit}
-                  onPaste={(e) => {
-                    e.preventDefault();
-                    return false;
-                  }}
+                //   onPaste={(e) => {
+                //     e.preventDefault();
+                //     return false;
+                //   }}
                   onChange={this.handleContentChange.bind(this)}
                 />
               </div>
             </div>
             <div className="content-right">
-              {
-                examType === 'practice' ? 
-                  <div className="ai-review">
-                    <Button
-                      className="btn"
-                      type="primary"
-                      shape="round"
-                      onClick={this.getAiReview.bind(this)}
-                      disabled={aiDetectionTimes === 0}
-                    >
-                      AI智能批改({aiDetectionTimes}/3)
-                    </Button>
-                  </div> : ''
-              }
+              {examType === "practice" ? (
+                <div className="ai-review">
+                  <Button
+                    className="btn"
+                    type="primary"
+                    shape="round"
+                    onClick={this.getAiReview.bind(this)}
+                    disabled={aiDetectionTimes === 0}
+                  >
+                    AI智能批改({aiDetectionTimes}/3)
+                  </Button>
+                </div>
+              ) : (
+                ""
+              )}
               <div className="content-tab">
-                <Tabs activeKey={activeKey} defaultActiveKey="2" onChange={this.handleTabChange.bind(this)}>
-                  {
-                    aiReview.aiComment?(
+                <Tabs
+                  activeKey={activeKey}
+                  defaultActiveKey="2"
+                  onChange={this.handleTabChange.bind(this)}
+                >
+                  {aiReview.aiComment ? (
                     <Tabs.TabPane tab="评语" key="1">
-                        <div className="content-demand-commit">
-                        <div className="commit-left">
-                            {aiReview.aiComment}
-                        </div>
+                      <div className="content-demand-commit">
+                        <div className="commit-left">{aiReview.aiComment}</div>
                         <div className="pp">
-                            <Progress
+                          <Progress
                             type="dashboard"
                             percent={aiReview.aiScore}
                             format={(percent) => `${percent}`}
-                            />
+                          />
                         </div>
-                        </div>
+                      </div>
                     </Tabs.TabPane>
-                    ):''
-                  }
+                  ) : (
+                    ""
+                  )}
                   <Tabs.TabPane tab="写作要求" key="2">
                     <div className="content-demand">
                       <div className="demand-fir">
@@ -374,27 +402,31 @@ export default class WritingDetail extends React.Component {
               </div>
               <div className="commit-error">
                 <div className="fir-line">
-                <div className="title">纠错</div>
-                <div className="count">{aiReview.sentenceComments.length}</div>
+                  <div className="title">纠错</div>
+                  <div className="count">
+                    {aiReview.sentenceComments.length}
+                  </div>
                 </div>
                 <div className="error-content">{this.errorItem()}</div>
-            </div>
+              </div>
             </div>
           </div>
           <div className="content-control">
-            <div className="control-text">（提交后无法再修改和编辑作文内容）</div>
+            <div className="control-text">
+              （提交后无法再修改和编辑作文内容）
+            </div>
             <Popconfirm
-                placement="top"
-                title={aiDetectionTimes === 0 ? "是否确认提交？提交后无法再修改和编辑作文内容。": `是否确认提交？您还有${aiDetectionTimes}次智能批改未用，且提交后提交后无法再修改和编辑作文内容`}
-                onConfirm={this.handleWritingSubmit.bind(this)}
-                okText="确认"
-                cancelText="取消"
+              placement="top"
+              title={
+                aiDetectionTimes === 0
+                  ? "是否确认提交？提交后无法再修改和编辑作文内容。"
+                  : `是否确认提交？您还有${aiDetectionTimes}次智能批改未用，且提交后提交后无法再修改和编辑作文内容`
+              }
+              onConfirm={this.handleWritingSubmit.bind(this)}
+              okText="确认"
+              cancelText="取消"
             >
-              <Button
-                  className="btn"
-                  type="primary"
-                  disabled={isSubmit}
-              >
+              <Button className="btn" type="primary" disabled={isSubmit}>
                 提交
               </Button>
             </Popconfirm>
